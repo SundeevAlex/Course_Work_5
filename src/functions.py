17ae6ea1f3b58a662500244cc419c3418329b944
Aleksandr Sundeev <navigator97@mail.ru>
2024-04-16 12:28:30 +0700

    add save_data_to_database but not working

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,5 @@
 import psycopg2
+from typing import Any
 
 
 def create_database(database_name: str, params: dict):
@@ -18,27 +19,58 @@ def create_database(database_name: str, params: dict):
 
 def create_tables(database_name: str, params: dict):
     """
-    Создание базы таблиц в БД
+    Создание таблиц в БД
     """
     conn = psycopg2.connect(dbname=database_name, **params)
     with conn.cursor() as cur:
         cur.execute("""
                 CREATE TABLE employers (
                     company_id SERIAL PRIMARY KEY,
-                    company_name VARCHAR UNIQUE
+                    company_name VARCHAR UNIQUE NOT NULL
                 )
             """)
 
     with conn.cursor() as cur:
         cur.execute("""
                 CREATE TABLE vacancies (
-                    vacancy_id SERIAL,
+                    vacancy_id SERIAL PRIMARY KEY,
+                    company_id INT REFERENCES employers(company_id),
                     vacancy_name TEXT NOT NULL,
                     salary INT,
-                    company_name TEXT REFERENCES employers(company_name) NOT NULL,
-                    vacancy_url VARCHAR NOT NULL,
-                    foreign key(company_name) references employers(company_name)
+                    vacancy_url VARCHAR NOT NULL
                 )
             """)
     conn.commit()
+    conn.close()
+
+
+def save_data_to_database(database_name, employers, vacancies, params: dict):
+    """
+    Сохранение данных о работодателях и вакансиях в базу данных.
+    """
+    conn = psycopg2.connect(dbname=database_name, **params)
+    with conn.cursor() as cursor:
+        for vacancy in vacancies:
+            print('***vacancies->', vacancies)
+            print('***vacancy->', vacancy)
+            print("***vacancy['employer']->", vacancy['employer'])
+            cursor.execute(
+                """
+                INSERT INTO employers (company_name)
+                VALUES (%s)
+                RETURNING company_id
+                """,
+                (vacancy['employer']))
+            # company_id = cur.fetchone()[0]
+    # with conn.cursor() as cur:
+    #     for employer in employers:
+    #         # print('emp--->', employer)
+    #         cur.execute(f"INSERT INTO employers(company_name) values ('{employer}')")
+    #         for vacancy in vacancies:
+    #             # print('vac--->', vacancy)
+    #             cur.execute(f"INSERT INTO vacancies(vacancy_name, salary, company_name, vacancy_url) values "
+    #                         f"('{vacancy['vacancy_name']}', '{int(vacancy['salary'])}', "
+    #                         f"'{vacancy['employer']}', '{vacancy['url']}')")
+
+    cursor.close()
     conn.close()
